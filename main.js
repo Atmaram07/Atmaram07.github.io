a2c3044aa21b03902988b8e587d4ac2db1d78327
Atmaram07 <ag4atmaramgreat@gmail.com>
2020-12-23 21:07:28 +0530

    updated the new version of website

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
 
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
-
-hamburger.addEventListener('click', ()=>{
-   //Animate Links
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
-
-    //Hamburger Animation
-    hamburger.classList.toggle("toggle");
-});
 var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
